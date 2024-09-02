7c329e9ec82aba02b19c0d5c0bec9bf273f6ec4c
Olya2125 <139589012+Olya2125@users.noreply.github.com>
2024-09-02 13:22:51 +0300

    added test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const bracketsMap = {};
+  const openBrackets = new Set();
+
+
+  bracketsConfig.forEach(([open, close]) => {
+      bracketsMap[close] = open;
+      openBrackets.add(open);
+  });
+
+  for (const char of str) {
+      if (openBrackets.has(char)) {
+
+          if (bracketsMap[char] === char) {
+              if (stack.length > 0 && stack[stack.length - 1] === char) {
+                  stack.pop();
+              } else {
+                  stack.push(char);
+              }
+          } else {
+              stack.push(char);
+          }
+      } else {
+        if (stack.length === 0 || stack.pop() !== bracketsMap[char]) {
+              return false;
+          }
+      }
+  }
+
+
+  return stack.length === 0;
+};
+
+
+
+
